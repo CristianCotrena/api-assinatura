2c090d5c7e51ee00cd0aeac318509ab1dcf4e4c1
ItaloAlisson <italoalisson15@gmail.com>
2023-09-13 20:42:41 -0300

    Adiciona api assinatura

---
+++ src/test/java/com/api/apiassinatura/ApiAssinaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.apiassinatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAssinaturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
